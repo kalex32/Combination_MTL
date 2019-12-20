8d4611783eb90eeaec3090021039c07a157c419c
Olexiy Koval <kalex32@ukr.net>
2019-12-21 01:38:05 +0200

    COmbination MTL

---
+++ src/Combination.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Combination {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int a = 5;
+        int quantity = 70;
+        int[][] comb = new int[quantity][a];
+        for (int i = 0; i < comb.length; i++) {
+            for (int j = 0; j < comb[i].length; j++) {
+                comb[i][j] = random.nextInt(12);
+
+            }
+        }
+        for (int i = 0; i < comb.length; i++) {
+            for (int j = 0; j < comb[i].length; j++) {
+                System.out.print(comb[i][j] + "\t");
+            }
+            System.out.println();
+        }
+//        System.out.println(Arrays.toString(comb));
+    }
+}
