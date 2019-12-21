c6ede114ccdf5e65682c734bebdee11cbd451081
Olexiy Koval <kalex32@ukr.net>
2019-12-22 00:57:48 +0200

    add char combinations

--- src/Combination.java
+++ src/Combination.java
@@ -6,11 +6,11 @@ public class Combination {
         Random random = new Random();
         int a = 5;
         int quantity = 70;
-        int[][] comb = new int[quantity][a];
+        String [][] comb = new String[quantity][a];
         for (int i = 0; i < comb.length; i++) {
             for (int j = 0; j < comb[i].length; j++) {
-                if (j == 0) comb[i][j] = random.nextInt(8);
-                else comb[i][j] = random.nextInt(12);
+                if (j == 0) comb[i][j] = String.valueOf(random.nextInt(7));
+                else comb[i][j] = String.valueOf(random.nextInt(11));
             }
         }
         for (int i = 0; i < comb.length; i++) {
